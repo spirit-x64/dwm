735287ca6b075385e49d3a0747c0bb7811359c89
Spirit <spirit@programmer.net>
2024-08-09 02:08:09 +0300

    set default tags for Godot, code firefox

--- config.def.h
+++ config.def.h
@@ -26,9 +26,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class               instance    title       tags mask     isfloating   monitor */
+	{ "Godot_ProjectList", NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Code - OSS",        NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Firefox",           NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
